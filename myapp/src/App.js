214d4a3fc21d7b476c18d404ef8f2e2b7cab750c
Shubhangi Ambade <chirag.jakhariya@rokigroup.co.uk>
2019-12-26 10:35:22 +0530

    Changes

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,24 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        { name: 'Dracula', id: 1 },
+        { name: 'Tejal', id: 2 },
+        { name: 'Abhishek', id: 3 },
+        { name: 'Mansi', id: 4 },
+        { name: 'Rahul', id: 5 },
+        { name: 'Shakuni(Jaydip)', id: 6 },
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-        <p>Shubhangi Ambade</p>
+        <h1>Monsters of India</h1>
+        {
+          this.state.monsters.map(monsters => {
+            return (<h2 key={monsters.id}>{monsters.name}</h2>)
+          })
+        }
       </div>
     );
   }
