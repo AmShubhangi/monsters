80f10f457b82a15cb52ae59a226159ba23132bdd
Shubhangi Ambade <chirag.jakhariya@rokigroup.co.uk>
2019-12-26 12:45:34 +0530

    Changes

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,15 +7,9 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        { name: 'Dracula', id: 1 },
-        { name: 'Tejal', id: 2 },
-        { name: 'Abhishek', id: 3 },
-        { name: 'Mansi', id: 4 },
-        { name: 'Rahul', id: 5 },
-        { name: 'Shakuni(Jaydip)', id: 6 },
-      ]
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
 
   componentDidMount() {
@@ -25,18 +19,25 @@ class App extends React.Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
     return (
       <div className="App">
         <h1>Monsters of India</h1>
+
+        <input type="search" onChange={e => this.setState({ searchField: e.target.value })
+        } placeholder="search  monsters" />
+
         {/* // Method 2 */}
-        <CardList monsters={this.state.monsters}>
-          {/*  Method 1 */}
-          {/* {
+        <CardList monsters={filteredMonsters} />
+        {/*  Method 1 */}
+        {/* {
             this.state.monsters.map(monsters => {
               return (<h2 key={monsters.id}>{monsters.name}</h2>)
             })
           } */}
-        </CardList>
       </div>
     );
   }
--- myapp/src/card/card.component.jsx
+++ myapp/src/card/card.component.jsx
@@ -4,7 +4,9 @@ import '../card/card.styles.css';
 export const Card = (props) => {
     return (
         <div className='card-containers'>
+            <img alt="monstes" src={`https://robohash.org/${props.monsters.id}.png?set=set1&size=180 x 180`} />
             <h1>{props.monsters.name}</h1>
-        </div>
+            <p>{props.monsters.email}</p>
+        </div >
     )
 }
