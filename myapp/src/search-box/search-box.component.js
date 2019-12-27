0f0e4442b8786da1cb540e456f229913da6cd20d
Shubhangi Ambade <shubhambade6897@gmail.com>
2019-12-27 18:11:37 +0530

    Updates

--- myapp/src/search-box/search-box.component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../search-box/search-box.styles.css';
-
-export const SearchBox = ({ placeholder, handleChange }) => {
-    return (<div>
-        <input type="search" className='search' onChange={handleChange}
-            placeholder={placeholder} />
-    </div>)
-}
---
+++ precache-manifest.31b4fdc9d97ba5f6830400cf5f14f0b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a2bd6defb1f4073f9c63fa72a8cdaf",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e914003a07f0f3074d10",
+    "url": "/monsters/static/css/main.3b7dd1de.chunk.css"
+  },
+  {
+    "revision": "03961aefbff84ff13ab7",
+    "url": "/monsters/static/js/2.923885a9.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/monsters/static/js/2.923885a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e914003a07f0f3074d10",
+    "url": "/monsters/static/js/main.0403f105.chunk.js"
+  },
+  {
+    "revision": "8bdf229af4cb29d7dd8a",
+    "url": "/monsters/static/js/runtime-main.bca33ad4.js"
+  }
+]);
